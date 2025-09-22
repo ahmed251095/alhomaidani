bd68cfa49290d9f0bbfcb8a9656f42294d77bd78
ahmed251095 <145964480+ahmed251095@users.noreply.github.com>
2025-09-22 23:18:31 +0300

    Add files via upload

---
+++ sale_customer_filter_quotations/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Customer Filter in Quotations",
+    "summary": "Show only customers in the customer field on quotations",
+    "version": "18.0.1.0.0",
+    "author": "Ahmed's Assistant",
+    "website": "https://example.com",
+    "license": "LGPL-3",
+    "category": "Sales",
+    "depends": ["sale"],
+    "data": [
+        "views/sale_order_view.xml",
+    ],
+    "installable": True,
+    "application": False,
+}
