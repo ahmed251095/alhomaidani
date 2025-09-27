28d454f9309a563f4c5879d86f577ee6bda060d3
ahmed251095 <145964480+ahmed251095@users.noreply.github.com>
2025-09-27 16:03:36 +0300

    Add files via upload

---
+++ sale_approval_flow_v2/wizard/reject_wizard.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from odoo import models, fields, _
+
+class SaleApprovalRejectWizard(models.TransientModel):
+    _name = "sale.approval.reject.wizard"
+    _description = "Reject Quotation Wizard"
+
+    sale_id = fields.Many2one("sale.order", required=True)
+    reason = fields.Text(required=True, string="Rejection Reason")
+
+    def action_reject(self):
+        self.ensure_one()
+        sale = self.sale_id
+        sale.write({
+            "approval_state":"rejected",
+            "rejection_reason": self.reason,
+        })
+        sale.message_post(body=_("تم رفض عرض السعر بواسطة %s.<br/>السبب: %s") % (self.env.user.display_name, self.reason))
+        return {"type":"ir.actions.act_window_close"}
